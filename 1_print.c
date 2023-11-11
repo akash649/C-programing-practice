bdec5baa60e5dde80a069783bc7101596066c8ca
Md Akash <akashnazmul649@gmail.com>
2023-11-12 01:16:18 +0600

    add

---
+++ 1_print.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(){
+    printf("akash\n\n"); // /n used for new line
+    printf("i am learning c");
+    return 0;
+}
---
+++ 2_variables.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(){
+    char name[] = "akash"; // store string use char
+    int age = 18;
+    float number = 5.5;
+    int x = 5,y =10,z = 20;
+    int c,d,e;
+    c = d = e = 50;
+    int a;
+    int b;
+    int sum;
+    a = 10;
+    b = 20;
+    sum = a*2;
+
+
+
+    // printf("\nthis is stirng number: %s", name );//print string use "%s" use "\n" for new line in output
+    // printf("\nthis is intger number: %i",age); //print int use "%d" or "%i" 
+    // printf("\nthis is float number : %f",number); //print float use "%f"
+    // printf("\n\n");
+    // printf("\nmy name is: %s \nmy age is : %i \nMy favoritte number is : %f",name,age,number);
+    printf("\n\nthis is sum value : %i",sum);
+    printf("\n\nthis is x+y+z value : %i",x + y + z);
+    return 0 ;
+}
